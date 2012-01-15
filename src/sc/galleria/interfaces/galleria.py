90f9d556ced7cce4bdacea240f9ecf96e94b8228
Cleber J Santos <cleber@cleberjsantos.com.br>
2012-01-15 01:27:53 -0200

    Updates

--- src/sc/galleria/interfaces/galleria.py
+++ src/sc/galleria/interfaces/galleria.py
@@ -18,42 +18,84 @@ class IGalleriaLayer(Interface):
     Marker Default browser layer this product.
     """
 
+class IGalleria(Interface):
+    """
+    """
+
+    def __init__(self, context, request,*args,**kwargs):
+        """ """
+
+    def settings(self):
+        """ """
+
+    def get_theme(self):
+        """ """
+
+    def portal_url(self):
+        """ """
+
+
 class IGalleriaSettings(Interface):
     """
      Option informations: http://galleria.io/docs/1.2/options/
     """
     autoplay = schema.Bool(title=u"Auto Play.",
                            description=u"Sets Galleria to play slidehow when initialized.",
-                           default=True)
+                           default=True,
+                           required=True,)
 
     showInf = schema.Bool(title=u"Show informations",
                           description=u"Toggles the caption.",
-                          default=True)
+                          default=True,
+                          required=True,)
+
+    image_width = schema.Int(title=u"Image width",
+                             description=u"Manually set a gallery width.",
+                             default=500,
+                             required=True,)
+
+    image_height = schema.Int(title=u"Image height",
+                              description=u"Manually set a gallery height.",
+                              default=500,
+                              required=True,)
+
+    imagePosition = schema.TextLine(title=u"Image css position",
+                                    description="Eg. 'top right' or '20% 100%'",
+                                    default='center'
+                                    required=True,)
+
+    lightbox = schema.Bool(title=u"Enabel lightbox",
+                               default=False,
+                               required=True,)
+
+
+    showCounting = schema.Bool(title=u"Show counting",
+                               description=u"Toggles the counter.",
+                               default=True,
+                               required=True,)
 
     transitions = schema.Choice(title=u"Transitions",
                           description=u"Defines what transition to use.",
                           default='fade',
-                          vocabulary=transitions)
+                          vocabulary=transitions,
+                          required=True,)
 
     transitionSpeed = schema.Int(title=u"Transition Speed", 
                                  description=u"Defines the speed of the transition.",
-                                 default=400)
+                                 default=400,
+                                 required=True,)
 
-    lightbox = schema.Bool(title=u"Enabel lightbox",
-                               default=False)
+    showimagenav = schema.bool(title=u"show image navigation",
+                               description=u"toggles the image navigation arrows.",
+                               default=True,
+                               required=True,)
 
-    image_width = schema.Int(title=u"Image width",
-                             description=u"Manually set a gallery width.",
-                             default=500)
-
-    image_height = schema.Int(title=u"Image height",
-                              description=u"Manually set a gallery height.",
-                              default=500)
-
-    showCounting = schema.Bool(title=u"Show counting",
-                               description=u"Toggles the counter.",
-                               default=True)
+    swipe = schema.bool(title=u"swipe",
+                               description=u"Enables a swipe movement for flicking through images on touch devices.",
+                               default=True,
+                               required=True,)
 
     debug = schema.Bool(title=u"Enable debug mode",
                         description=u"Set this to false to prevent debug messages.",
-                        default=False)
+                        default=False,
+                        required=True,)
