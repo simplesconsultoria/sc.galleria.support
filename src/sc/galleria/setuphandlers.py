4a8b6846985a81c46e19e3a81db80fe2ec9c3c9a
Cleber J Santos <cleber@cleberjsantos.com.br>
2012-01-25 14:21:32 -0200

    Steps update

--- src/sc/galleria/setuphandlers.py
+++ src/sc/galleria/setuphandlers.py
@@ -10,10 +10,21 @@ _PROJECT = 'sc.galleria'
 _PROFILE_ID = 'sc.galleria:default'
 
 
+def install(context):
+
+    # Ordinarily, GenericSetup handlers check for the existence of XML files.
+    # Here, we are not parsing an XML file, but we use this text file as a
+    # flag to check that we actually meant for this import step to be run.
+    # The file is found in profiles/default.
+
+    if context.readDataFile('sc.galleria_default.txt') is None:
+        return
+
+
 def run_upgrades(context):
     ''' Run Upgrade steps
     '''
-    if context.readDataFile('sc.galleria-default.txt') is None:
+    if context.readDataFile('sc.galleria_default.txt') is None:
         return
     logger = logging.getLogger(_PROJECT)
     site = context.getSite()
@@ -30,3 +41,14 @@ def run_upgrades(context):
                                                           _PROFILE_ID)
             setup_tool.setLastVersionForProfile(_PROFILE_ID, oStep.dest)
             logger.info(msg)
+
+def uninstall(context):
+    ''' Run uninstall steps
+    '''
+
+    if context.readDataFile('sc.galleria_uninstall.txt') is None:
+        return
+
+    portal = context.getSite()
+    portal_conf = getToolByName(portal, 'portal_controlpanel')
+    portal_conf.unregisterConfiglet('@@galleria-settings')
