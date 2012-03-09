be82d958f8a106e3744b44f4730620e251cc185e
hvelarde <hector.velarde@gmail.com>
2012-03-09 14:00:18 -0300

    fixed package declaration

--- setup.py
+++ setup.py
@@ -1,39 +1,50 @@
 # -*- coding:utf-8 -*-
+
 import os
 from setuptools import setup, find_packages
 
 version = '0.5'
+long_description = open("README.rst").read() + "\n" + \
+                   open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "HISTORY.txt")).read()
 
 setup(name='sc.galleria.support',
       version=version,
       description="Simple gallery for plone based in http://galleria.io",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=long_description,
       classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='python simples_consultoria plone galleria gallery',
       author='Simples Consultoria',
       author_email='products@simplesconsultoria.com.br',
       url='http://www.simplesconsultoria.com.br',
-      license='gpl',
+      license='GPL',
       packages=find_packages('src'),
-      package_dir = {'': 'src'},
-      namespace_packages=['sc',
-                         'sc.galleria'],
+      package_dir={'': 'src'},
+      namespace_packages=['sc', 'sc.galleria'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-          'plone.app.registry',
-      ],
+        'setuptools',
+        'plone.app.registry',
+        ],
       extras_require={
         'test': ['plone.app.testing'],
         },
       entry_points="""
-      # -*- Entry points: -*-
-
       [z3c.autoinclude.plugin]
       target = plone
       """,
