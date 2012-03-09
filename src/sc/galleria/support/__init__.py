41be48bde3bdcb2a3a4ec701c8588aef3b9f456b
hvelarde <hector.velarde@gmail.com>
2012-03-09 14:19:39 -0300

    we don't really need an initialize method

--- src/sc/galleria/support/__init__.py
+++ src/sc/galleria/support/__init__.py
@@ -3,6 +3,3 @@
 from zope.i18nmessageid import MessageFactory as BaseMessageFactory
 
 MessageFactory = BaseMessageFactory('sc.galleria.support')
-
-def initialize(context):
-    """Initializer called when used as a Zope 2 product."""
