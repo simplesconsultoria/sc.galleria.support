a18df1df2afb9e83e870ac23b7fab342fa84d500
Alejandro Pereira <alepereira86@gmail.com>
2012-03-15 19:48:04 -0300

    #4264 adding registry and browser view  tests cases and expanding documentation in README.rst

---
+++ src/sc/galleria/support/tests/test_browserview.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+
+import unittest2 as unittest
+
+from sc.galleria.support.testing import INTEGRATION_TESTING
+
+
+class BrowserViewTest(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+        self.user = self.portal['portal_membership'].getAuthenticatedMember()
+
+    def test_galleria_picasauserandid_no_link(self):
+        from sc.galleria.support.browser.galleria import Galleria
+        galleria = Galleria(self.user, self.request)
+        self.assertEquals(galleria.galleria_picasauserandid(), None)
+
+    def test_galleria_flickrid_no_link(self):
+        from sc.galleria.support.browser.galleria import Galleria
+        galleria = Galleria(self.user, self.request)
+        self.assertEquals(galleria.galleria_flickrid(), None)
+
+    def test_galleria_picasauserandid_with_link(self):
+        from Products.ATContentTypes.content.link import ATLink
+        from sc.galleria.support.browser.galleria import Galleria
+        link = ATLink(self.user)
+        link.setRemoteUrl('https://picasaweb.google.com/user_id/galleria_id')
+        galleria = Galleria(link, self.request)
+        self.assertEquals(galleria.galleria_picasauserandid(),
+                          ('user_id', 'galleria_id'))
+
+    def test_galleria_flickrid_with_link(self):
+        from Products.ATContentTypes.content.link import ATLink
+        from sc.galleria.support.browser.galleria import Galleria
+        link = ATLink(self.user)
+        link.setRemoteUrl(
+            'http://www.flickr.com/photos/user_id/sets/galleria_id/')
+        galleria = Galleria(link, self.request)
+        self.assertEquals(galleria.galleria_flickrid(), 'galleria_id')
+
+    def test_portal_url(self):
+        from sc.galleria.support.browser.galleria import Galleria
+        galleria = Galleria(self.user, self.request)
+        self.assertEquals(galleria.portal_url(), self.portal.absolute_url())
+
+    def test_getThumbnails(self):
+        from sc.galleria.support.browser.galleria import Galleria
+        galleria = Galleria(self.user, self.request)
+        self.assertEquals(galleria.getThumbnails(), 'true')
+
+    def js_values_no_link(self, view):
+        from sc.galleria.support.interfaces import IGeneralSettings
+        result = {
+            "jQuery": "('%s')" %
+                    str(IGeneralSettings.get('selector').default),
+            "width: ": str(IGeneralSettings.get('gallery_width').default),
+            "height: ": str(IGeneralSettings.get('gallery_height').default),
+            "autoplay: ":
+                    str(IGeneralSettings.get('autoplay').default).lower(),
+            "showInfo: ": str(IGeneralSettings.get('showInf').default).lower(),
+            "imagePosition: ": "'%s'" %
+                    str(IGeneralSettings.get('imagePosition').default),
+            "transition: ": "'%s'" %
+                    IGeneralSettings.get('transitions').default,
+            "transitionSpeed: ":
+                    str(IGeneralSettings.get('transitionSpeed').default),
+            "lightbox: ":
+                    str(IGeneralSettings.get('lightbox').default).lower(),
+            "showCounter: ":
+                    str(IGeneralSettings.get('showCounting').default).lower(),
+            "showImagenav: ":
+                    str(IGeneralSettings.get('showimagenav').default).lower(),
+            "swipe: ": str(IGeneralSettings.get('swipe').default).lower(),
+            "dummy: ": "'%s'" % (view.portal_url() + \
+                        '/++resource++galleria-images/dummy.png'),
+            "thumbnails: ": view.getThumbnails(),
+            "debug: ": str(IGeneralSettings.get('debug').default).lower(),
+            }
+        return result
+
+    def test_galleriajs_no_link(self):
+        from sc.galleria.support.browser.galleria import Galleria
+        galleria = Galleria(self.user, self.request)
+        js = galleria.galleriajs()
+        values = self.js_values_no_link(galleria)
+        for key in values.keys():
+            self.assertTrue(key + values[key] in js)
+
+    def js_values_with_link_flickr(self, view):
+        from sc.galleria.support.interfaces import IGeneralSettings
+        result = {
+            "jQuery": "('%s')" %
+                    str(IGeneralSettings.get('selector').default),
+            "set = ": "'%s'" % str(view.galleria_flickrid()),
+            "max: ": str(view.flickrplugin.flickr_max),
+            "description: ": str(view.flickrplugin.flickr_desc).lower(),
+            "width: ": str(IGeneralSettings.get('gallery_width').default),
+            "height: ": str(IGeneralSettings.get('gallery_height').default),
+            "autoplay: ":
+                    str(IGeneralSettings.get('autoplay').default).lower(),
+            }
+        return result
+
+    def test_galleriajs_with_link_flickr(self):
+        from Products.ATContentTypes.content.link import ATLink
+        from sc.galleria.support.browser.galleria import Galleria
+        link = ATLink(self.user)
+        link.setRemoteUrl(
+            'http://www.flickr.com/photos/user_id/sets/galleria_id/')
+        galleria = Galleria(link, self.request)
+        galleria.flickrplugin.flickr = True
+        js = galleria.galleriajs()
+        values = self.js_values_with_link_flickr(galleria)
+        for key in values.keys():
+            self.assertTrue(key + values[key] in js)
+
+    def js_values_with_link_picasa(self, view):
+        from sc.galleria.support.interfaces import IGeneralSettings
+        result = {
+            "jQuery": "('%s')" %
+                    str(IGeneralSettings.get('selector').default),
+            "max: ": str(view.flickrplugin.flickr_max),
+            "description: ": str(view.flickrplugin.flickr_desc).lower(),
+            "picasa.useralbum": """( '%s', '%s',""" % \
+                            (str(view.galleria_picasauserandid()[0]),
+                                str(view.galleria_picasauserandid()[1])),
+            "width: ": str(IGeneralSettings.get('gallery_width').default),
+            "height: ": str(IGeneralSettings.get('gallery_height').default),
+            "autoplay: ":
+                    str(IGeneralSettings.get('autoplay').default).lower(),
+            }
+        return result
+
+    def test_galleriajs_with_link_picasa(self):
+        from Products.ATContentTypes.content.link import ATLink
+        from sc.galleria.support.browser.galleria import Galleria
+        link = ATLink(self.user)
+        link.setRemoteUrl('https://picasaweb.google.com/user_id/galleria_id')
+        galleria = Galleria(link, self.request)
+        galleria.picasaplugin.picasa = True
+        js = galleria.galleriajs()
+        values = self.js_values_with_link_picasa(galleria)
+        for key in values.keys():
+            self.assertTrue(key + values[key] in js)
+
+
+def test_suite():
+    return unittest.defaultTestLoader.loadTestsFromName(__name__)
--- src/sc/galleria/support/tests/test_controlpanel.py
+++ src/sc/galleria/support/tests/test_controlpanel.py
@@ -3,10 +3,12 @@
 import unittest2 as unittest
 
 from zope.component import getMultiAdapter
+from zope.component import getUtility
 
 from plone.app.testing import TEST_USER_ID
 from plone.app.testing import logout
 from plone.app.testing import setRoles
+from plone.registry.interfaces import IRegistry
 
 from Products.CMFCore.utils import getToolByName
 
@@ -20,6 +22,7 @@ class RegistryTest(unittest.TestCase):
     def setUp(self):
         self.portal = self.layer['portal']
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.registry = getUtility(IRegistry)
 
     def test_controlpanel_view(self):
         view = getMultiAdapter((self.portal, self.portal.REQUEST),
@@ -38,6 +41,193 @@ class RegistryTest(unittest.TestCase):
         actions = [a.getAction(self)['id']
                    for a in self.controlpanel.listActions()]
         self.assertTrue('galleria' in actions)
+
+
+class RecordsGeneralSettings(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.registry = getUtility(IRegistry)
+
+    def test_record_autoplay(self):
+        from sc.galleria.support.interfaces import IGeneralSettings
+        record_autoplay = self.registry.records[
+            'sc.galleria.support.interfaces.IGeneralSettings.autoplay']
+        self.failUnless('autoplay' in IGeneralSettings)
+        self.assertEquals(record_autoplay.value, True)
+
+    def test_record_showInf(self):
+        from sc.galleria.support.interfaces import IGeneralSettings
+        record_showInf = self.registry.records[
+            'sc.galleria.support.interfaces.IGeneralSettings.showInf']
+        self.failUnless('showInf' in IGeneralSettings)
+        self.assertEquals(record_showInf.value, True)
+
+    def test_record_gallery_width(self):
+        from sc.galleria.support.interfaces import IGeneralSettings
+        record_gallery_width = self.registry.records[
+            'sc.galleria.support.interfaces.IGeneralSettings.gallery_width']
+        self.failUnless('gallery_width' in IGeneralSettings)
+        self.assertEquals(record_gallery_width.value, 500)
+
+    def test_record_gallery_height(self):
+        from sc.galleria.support.interfaces import IGeneralSettings
+        record_gallery_height = self.registry.records[
+            'sc.galleria.support.interfaces.IGeneralSettings.gallery_height']
+        self.failUnless('gallery_height' in IGeneralSettings)
+        self.assertEquals(record_gallery_height.value, 500)
+
+    def test_record_imagePosition(self):
+        from sc.galleria.support.interfaces import IGeneralSettings
+        record_imagePosition = self.registry.records[
+            'sc.galleria.support.interfaces.IGeneralSettings.imagePosition']
+        self.failUnless('imagePosition' in IGeneralSettings)
+        self.assertEquals(record_imagePosition.value, u'center')
+
+    def test_record_lightbox(self):
+        from sc.galleria.support.interfaces import IGeneralSettings
+        record_lightbox = self.registry.records[
+            'sc.galleria.support.interfaces.IGeneralSettings.lightbox']
+        self.failUnless('lightbox' in IGeneralSettings)
+        self.assertEquals(record_lightbox.value, False)
+
+    def test_record_showCounting(self):
+        from sc.galleria.support.interfaces import IGeneralSettings
+        record_showCounting = self.registry.records[
+            'sc.galleria.support.interfaces.IGeneralSettings.showCounting']
+        self.failUnless('showCounting' in IGeneralSettings)
+        self.assertEquals(record_showCounting.value, True)
+
+    def test_record_transitions(self):
+        from sc.galleria.support.interfaces import IGeneralSettings
+        record_transitions = self.registry.records[
+            'sc.galleria.support.interfaces.IGeneralSettings.transitions']
+        self.failUnless('transitions' in IGeneralSettings)
+        self.assertEquals(record_transitions.value, u'fade')
+
+    def test_record_transitionSpeed(self):
+        from sc.galleria.support.interfaces import IGeneralSettings
+        record_transitionSpeed = self.registry.records[
+            'sc.galleria.support.interfaces.IGeneralSettings.transitionSpeed']
+        self.failUnless('transitionSpeed' in IGeneralSettings)
+        self.assertEquals(record_transitionSpeed.value, 400)
+
+    def test_record_showimagenav(self):
+        from sc.galleria.support.interfaces import IGeneralSettings
+        record_showimagenav = self.registry.records[
+            'sc.galleria.support.interfaces.IGeneralSettings.showimagenav']
+        self.failUnless('showimagenav' in IGeneralSettings)
+        self.assertEquals(record_showimagenav.value, True)
+
+    def test_record_swipe(self):
+        from sc.galleria.support.interfaces import IGeneralSettings
+        record_swipe = self.registry.records[
+            'sc.galleria.support.interfaces.IGeneralSettings.swipe']
+        self.failUnless('swipe' in IGeneralSettings)
+        self.assertEquals(record_swipe.value, True)
+
+    def test_record_selector(self):
+        from sc.galleria.support.interfaces import IGeneralSettings
+        record_selector = self.registry.records[
+            'sc.galleria.support.interfaces.IGeneralSettings.selector']
+        self.failUnless('selector' in IGeneralSettings)
+        self.assertEquals(record_selector.value, u"#content")
+
+    def test_record_thumbnails(self):
+        from sc.galleria.support.interfaces import IGeneralSettings
+        record_thumbnails = self.registry.records[
+            'sc.galleria.support.interfaces.IGeneralSettings.thumbnails']
+        self.failUnless('thumbnails' in IGeneralSettings)
+        self.assertEquals(record_thumbnails.value, u'show')
+
+    def test_record_debug(self):
+        from sc.galleria.support.interfaces import IGeneralSettings
+        record_debug = self.registry.records[
+            'sc.galleria.support.interfaces.IGeneralSettings.debug']
+        self.failUnless('debug' in IGeneralSettings)
+        self.assertEquals(record_debug.value, False)
+
+
+class RecordsFlickrPlugin(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.registry = getUtility(IRegistry)
+
+    def test_record_flickr(self):
+        from sc.galleria.support.interfaces import IFlickrPlugin
+        record_flickr = self.registry.records[
+            'sc.galleria.support.interfaces.IFlickrPlugin.flickr']
+        self.failUnless('flickr' in IFlickrPlugin)
+        self.assertEquals(record_flickr.value, False)
+
+    def test_record_flickr_max(self):
+        from sc.galleria.support.interfaces import IFlickrPlugin
+        record_flickr_max = self.registry.records[
+            'sc.galleria.support.interfaces.IFlickrPlugin.flickr_max']
+        self.failUnless('flickr_max' in IFlickrPlugin)
+        self.assertEquals(record_flickr_max.value, 20)
+
+    def test_record_flickr_desc(self):
+        from sc.galleria.support.interfaces import IFlickrPlugin
+        record_flickr_desc = self.registry.records[
+            'sc.galleria.support.interfaces.IFlickrPlugin.flickr_desc']
+        self.failUnless('flickr_desc' in IFlickrPlugin)
+        self.assertEquals(record_flickr_desc.value, False)
+
+
+class RecordsPicasaPlugin(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.registry = getUtility(IRegistry)
+
+    def test_record_picasa(self):
+        from sc.galleria.support.interfaces import IPicasaPlugin
+        record_picasa = self.registry.records[
+            'sc.galleria.support.interfaces.IPicasaPlugin.picasa']
+        self.failUnless('picasa' in IPicasaPlugin)
+        self.assertEquals(record_picasa.value, False)
+
+    def test_record_picasa_max(self):
+        from sc.galleria.support.interfaces import IPicasaPlugin
+        record_picasa_max = self.registry.records[
+            'sc.galleria.support.interfaces.IPicasaPlugin.picasa_max']
+        self.failUnless('picasa_max' in IPicasaPlugin)
+        self.assertEquals(record_picasa_max.value, 20)
+
+    def test_record_picasa_desc(self):
+        from sc.galleria.support.interfaces import IPicasaPlugin
+        record_picasa_desc = self.registry.records[
+            'sc.galleria.support.interfaces.IPicasaPlugin.picasa_desc']
+        self.failUnless('picasa_desc' in IPicasaPlugin)
+        self.assertEquals(record_picasa_desc.value, False)
+
+
+class RecordsHistoryPlugin(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.registry = getUtility(IRegistry)
+
+    def test_record_history(self):
+        from sc.galleria.support.interfaces import IHistoryPlugin
+        record_history = self.registry.records[
+            'sc.galleria.support.interfaces.IHistoryPlugin.history']
+        self.failUnless('history' in IHistoryPlugin)
+        self.assertEquals(record_history.value, False)
 
 
 def test_suite():
