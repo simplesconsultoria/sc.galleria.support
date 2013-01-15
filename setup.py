958aed5d87fcf25753f8d96902a1478563fc81d6
Cleber J Santos <cleber@cleberjsantos.com.br>
2013-01-15 11:55:22 -0200

    Fix and refactor buildout

--- setup.py
+++ setup.py
@@ -43,9 +43,14 @@ setup(name='sc.galleria.support',
       install_requires=[
         'setuptools',
         'plone.app.registry',
+        'collective.js.galleria',
         ],
       extras_require={
-        'test': ['plone.app.testing'],
+        'test': [
+          'plone.app.testing',
+          'robotsuite',
+          'robotframework-selenium2library',
+          ],
         },
       entry_points="""
       [z3c.autoinclude.plugin]
